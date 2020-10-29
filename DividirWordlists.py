d8ae373b14b194f3c7ea84ec35de32a23a03dc4a
yagho38 <yagho38@gmail.com>
2020-10-29 16:10:33 -0300

    Version sla

--- DividirWordlists.py
+++ DividirWordlists.py
@@ -1,12 +1,16 @@
+# -*- coding: utf-8 -*-
+
 from os import path, chdir, listdir, getcwd, system
 import sys
 
-with open("/home/yagho/Documentos/rockyou.txt") as results:
+with open("/usr/share/wordlists/rockyou.txt") as results:
     original = results.readlines()
-file2 = open('/home/yagho/Documentos/lista13.txt', 'w+')
+nomeDoArquivo = str(input("Nome do arquivo: "))
+numero1 = int(input("Digite o numero inicial: "))
+numero2 = int(input("Digite o numero final: "))
+file2 = open(("/home/hack/Documentos/rockyoupartida/" + str(nomeDoArquivo)), "w+")
 counter = 0
-
 for line in original:
     counter += 1
-    if(counter >= 1200000 and counter <= 1300000):
-        file2.write(line + '\n')
+    if(counter >= numero1 and counter <= numero2):
+        file2.write(line + "\n")
