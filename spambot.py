70336e87e0e6027c96cb4e56a8c70a47b113fc62
Yagho38 <yagho38@gmail.com>
2020-11-11 11:06:18 -0300

    Novo sniffer.py

--- spambot.py
+++ spambot.py
@@ -1,19 +1,10 @@
 import pyautogui, time
-from threading import Thread
 
 time.sleep(5)
 
 f = open("spam.txt", "r")
 sla = f.readlines()
-def mandar1():
-	while True:
-		pyautogui.press('shift')
-def mandar2():
-	while True:
+while True:
+	for word in sla:
+		pyautogui.typewrite(word)
 		pyautogui.press('enter')
-
-t_mandar1 = Thread(target=mandar1)
-t_mandar1.start()
-
-t_mandar2 = Thread(target=mandar2)
-t_mandar2.start()
